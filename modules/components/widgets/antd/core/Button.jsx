732af29b3c3ed54c193f1075313f97cf8bacac75
mzarate-speedeon <mzarate@speedeondata.com>
2023-12-04 08:53:42 -0700

    testing changes

--- modules/components/widgets/antd/core/Button.jsx
+++ modules/components/widgets/antd/core/Button.jsx
@@ -40,6 +40,6 @@ export default ({type, onClick, label, readonly, config: {settings}}) => {
       onClick={onClick}
       size={renderSize}
       disabled={readonly}
-    >{btnLabel} ZZZZZZ</Button>
+    >{btnLabel} XXYYZZ</Button>
   );
 };
