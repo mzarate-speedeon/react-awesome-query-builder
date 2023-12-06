87f63e12695f03f96554407119df80b7baa5aff9
mzarate-speedeon <mzarate@speedeondata.com>
2023-12-06 14:14:37 -0700

    import useEffect

--- modules/components/widgets/antd/value/MultiSelect.jsx
+++ modules/components/widgets/antd/value/MultiSelect.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, useRef, useState } from "react";
+import React, { PureComponent, useRef, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Select } from "antd";
 import {calcTextWidth, SELECT_WIDTH_OFFSET_RIGHT} from "../../../../utils/domUtils";
